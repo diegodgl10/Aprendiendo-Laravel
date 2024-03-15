7c0714cf1e6cbc22f515378cfe0d8815fdef3d25
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-03-14 23:08:40 -0600

    hasta video 37

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,13 @@ use Illuminate\Support\Facades\Route;
  * Redireccion a la pagina inicial.
  */
 Route::get('/', function () {
-    $posts = Post::latest()->get();
+    $posts = Post::latest();
+    if (request('search')) {
+        $posts->where('title', 'like', '%' . request('search') . '%')
+        ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
     return view('posts', [
-        'posts' => $posts,
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
