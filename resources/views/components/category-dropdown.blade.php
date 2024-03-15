71172f54f3ba23167a6ed8904284979754e4c4e3
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-03-15 13:22:47 -0600

    hasta video 44

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -9,13 +9,13 @@
     </x-slot>
 
     <x-dropdown-item
-        href="/"
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
         :active="request()->routeIs('home')">
         Todas
     </x-dropdown-item>
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug; }}&{{ http_build_query(request()->except('category')) }}"
+            href="/?category={{ $category->slug; }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active="request()->is('categories/' . $category->slug)">
             {!! ucwords($category->name) !!}
         </x-dropdown-item>
