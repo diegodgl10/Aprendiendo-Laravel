12f7c4963f6029a283199b2dbbec8be63380e867
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-03-15 18:56:10 -0600

    hasta video 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,7 +16,12 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -31,7 +36,12 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required>
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -46,7 +56,12 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required>
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -62,6 +77,10 @@
                         name="password"
                         id="password"
                         required>
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -71,6 +90,16 @@
                         Aceptar
                     </button>
                 </div>
+
+                {{--
+                @if($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+                --}}
             </form>
         </main>
     </section>
