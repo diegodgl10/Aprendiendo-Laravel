51b5a1df51ead1c28685f06567b9f0f7eeae50a5
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-03-15 21:02:27 -0600

    hasta video 50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Bienvenido de nuevo');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Los datos no se pudieron verificar'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
