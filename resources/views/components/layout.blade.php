8bdea845670d306b3cd35e3216707ea5d2b65c68
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-03-15 20:25:42 -0600

    hasta video 49

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,8 +15,21 @@
                 </a>
             </div>
 
-            <div class="mt-8 md:mt-0">
-                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+            <div class="mt-8 md:mt-0 flex items-center">
+                @auth
+                    <a class="text-xs font-bold uppercase">Bienvenido, {{ auth()->user()->name}}</a>
+
+                    <form
+                        method="POST"
+                        action="/logout"
+                        class="text-xs font-semibold text-blue-500 ml-6">
+                        @csrf
+                        <button type="submit">Salir</button>
+                    </form>
+                @else
+                    <a href="/register" class="text-xs font-bold uppercase">Registrarse</a>
+                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Ingresar</a>
+                @endauth
 
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
